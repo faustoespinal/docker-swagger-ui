b3a31c46e8d7a34ca82bf5fed64b287f4287c959
faustoespinal <faustoespinal@gmail.com>
2017-10-01 20:58:54 -0500

    Added examples from swagger hub

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM swaggerapi/swagger-ui
 
-RUN mkdir /usr/share/nginx/html/api
+RUN  mkdir /usr/share/nginx/html/api
+
 COPY api /usr/share/nginx/html/api
 COPY api-index.html /usr/share/nginx/html
 COPY icon-api.png /usr/share/nginx/html
 COPY api-config.js /usr/share/nginx/html
 COPY handlebars-v4.0.10.js /usr/share/nginx/html
 
-RUN ls -ls 
 RUN ls -ls /usr/share/nginx/html/api
-
 
--- api-config.js
+++ api-config.js
@@ -8,8 +8,11 @@ $(function() {
     // This is the default context, which is passed to the template
     var context = {
         api: [
+            { name: 'Simple', url: "http://localhost:8080/?url=http://localhost:8080/api/simple-api.yaml" },
             { name: 'Pets-1', url: "http://localhost:8080/?url=http://localhost:8080/api/pets-1.yaml" },
             { name: 'Pets-2', url: "http://localhost:8080/?url=http://localhost:8080/api/pets-2.yaml" },
+            { name: 'IoT', url: "http://localhost:8080/?url=http://localhost:8080/api/iot.yaml" },
+            { name: 'Oauth sample', url: "http://localhost:8080/?url=http://localhost:8080/api/oauth-api.yaml" },
             { name: 'Pets-3', url: "http://localhost:8080/?url=http://localhost:8080/api/pets-1.yaml" },
         ]
     };
